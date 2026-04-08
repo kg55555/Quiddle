f5f611fa41bbc585427e324aba60f2a2ceefe496
Kevin Guo <kg55555@users.noreply.github.com>
2026-04-08 16:24:26 -0700

    fixing tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.js'],
-  collectCoverageFrom: ['routes/*.js', 'util/*.js', 'middleware/*.js', 'util/*.js'],
+  collectCoverageFrom: ['routes/*.js', 'util/*.js', 'middleware/*.js'],
+  globalTeardown: './tests/teardown.js',
 };
